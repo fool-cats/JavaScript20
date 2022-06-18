975b9ec1d36e0e81d1bff1d93207a90d4f0cfb7c
fool-cats <3331335256@qq.com>
2022-06-18 23:46:32 +0800

    fourth miniproject

---
+++ 4-Rate exchange calculator/script.js
@@ -0,0 +1,44 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+
+// fetch exchange rates and update dom
+function calculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    // 1. Using the fetch api to fetch the data 
+    fetch(` https://v6.exchangerate-api.com/v6/515f84838aa56532008d15e7/latest/${currency_one}`)
+    // 2. convert the data to JSON format
+    .then(res => res.json())
+    // 3. process the data
+    .then(data => {
+        // console.log(data);
+        const rate = data.conversion_rates[currency_two];
+        // console.log(rate);
+        rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+        amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    })
+}
+
+// event listener
+swap.addEventListener('click', function (event) {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value
+    currencyEl_two.value = temp;
+
+    // recalculate
+    calculate();
+})
+
+
+currencyEl_one.addEventListener('change', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+amountEl_two.addEventListener('input', calculate);
+
